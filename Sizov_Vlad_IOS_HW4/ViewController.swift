4f036de8768e95b9592242c6f3ca6a56dd47d938
Vlad Sizov <vladislav.sizov.2002@gmail.com>
2021-10-29 15:49:06 +0300

    Initial Commit

---
+++ Sizov_Vlad_IOS_HW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sizov_Vlad_IOS_HW4
+//
+//  Created by user on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sizov_Vlad_IOS_HW4Tests/Sizov_Vlad_IOS_HW4Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Sizov_Vlad_IOS_HW4Tests.swift
+//  Sizov_Vlad_IOS_HW4Tests
+//
+//  Created by user on 29.10.2021.
+//
+
+import XCTest
+@testable import Sizov_Vlad_IOS_HW4
+
+class Sizov_Vlad_IOS_HW4Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
